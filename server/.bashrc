961ccfd740f996663089b86f1c6fb200fffda641
Warin Laocharoen <chue@abctech-thailand.com>
2016-12-26 11:30:25 +0700

    server config

---
+++ server/.bashrc
@@ -0,0 +1,8 @@
+# Set variables in .bashrc file
+
+# don't forget to change your path correctly!
+
+export GOPATH=/usr/local/Cellar/go/1.7.4
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
---
+++ server/server.go
@@ -0,0 +1,29 @@
+package main
+
+import "github.com/kataras/iris"
+
+func main() {
+
+	// http://localhost:5700/api/user/42
+	// Method: "GET"
+	iris.Get("/api/user/:id", func(ctx *iris.Context) {
+
+		// take the :id from the path, parse to integer
+		// and set it to the new userID local variable.
+		userID, _ := ctx.ParamInt("id")
+
+		// userRepo, imaginary database service <- your only job.
+		user := userRepo.GetByID(userID)
+
+		// send back a response to the client,
+		// .JSON: content type as application/json; charset="utf-8"
+		// iris.StatusOK: with 200 http status code.
+		//
+		// send user as it is or make use of any json valid golang type,
+		// like the iris.Map{"username" : user.Username}.
+		ctx.JSON(iris.StatusOK, user)
+
+	})
+
+	iris.Listen("localhost:5700")
+}
