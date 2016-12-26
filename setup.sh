a577a6baa2f168234d156ea8e4ed34cc8cd5b00a
Pongsan Sayampol <aon@telenordigital.com>
2016-12-26 19:19:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-go get github.com/codegangsta/gin
+go get -u golang.org/x/tools/cmd/goimports
+
+go get -u github.com/golang/lint/golint
+
+go get -u github.com/tools/godep
+
+go get -u github.com/codegangsta/gin
