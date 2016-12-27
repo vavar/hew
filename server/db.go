388b7062f38feaa97d3f15c0bb8360ad82dfc37c
Pongsan Sayampol <aon@telenordigital.com>
2016-12-27 10:46:55 +0700

    Update godep dependency.

--- server/db.go
+++ server/db.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/postgres"
-    "github.com/spf13/viper"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/spf13/viper"
 )
 
 type Database struct {
-    DB *gorm.DB
+	DB *gorm.DB
 }
 
 func InitDBConnection() *Database {
-    host := viper.GetString("db.host")
-    user := viper.GetString("db.user")
-    dbname := viper.GetString("db.dbname")
-    dbConfig := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable", host, user, dbname)
-    db, err := gorm.Open("postgres", dbConfig)
-    if err != nil {
-        panic(fmt.Errorf("Failed to initiate a connection to the database: %s", err))
-    }
-    defer db.Close()
+	host := viper.GetString("db.host")
+	user := viper.GetString("db.user")
+	dbname := viper.GetString("db.dbname")
+	dbConfig := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable", host, user, dbname)
+	db, err := gorm.Open("postgres", dbConfig)
+	if err != nil {
+		panic(fmt.Errorf("Failed to initiate a connection to the database: %s", err))
+	}
+	defer db.Close()
 
-    return &Database{db}
+	return &Database{db}
 }
