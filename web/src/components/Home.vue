7c6198115d2f1ccf1cf5f9d3900efad879832d5d
Ken Thanmongkol <ken@abctech-thailand.com>
2016-12-28 10:33:54 +0700

    Add missing files

---
+++ web/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Dashboard</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ web/src/components/Order.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="order">
+    <h1>Today Order</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'order',
+  data() {},
+};
+</script>
