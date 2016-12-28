3082b30733a45b364eb2176b231fcffa7d857cb2
nropatas <sam@abctech-thailand.com>
2016-12-28 11:00:21 +0700

    Update User and Organization Services

--- server/user.go
+++ server/user.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"gopkg.in/gin-gonic/gin.v1"
 )
@@ -18,23 +19,89 @@ func NewUserService(db *Database) *UserService {
 
 //GetByID - User Profile by ID
 func (service *UserService) GetByID(c *gin.Context) {
-	// org := &Organization{1, "test"}
-	// jsonObject := &User{1, "chue", "a@a.com", org}
-	c.JSON(http.StatusOK, gin.H{})
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err,
+		})
+		return
+	}
+
+	var user User
+	if err = service.DB.FindUserByID(&user, id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 //AddUser - User Profile by ID
 func (service *UserService) AddUser(c *gin.Context) {
-	// org := &Organization{1, "test"}
-	// jsonObject := &User{1, "chue", "a@a.com", org}
-	c.JSON(http.StatusOK, gin.H{})
+	var json User
+	if err := c.BindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err,
+		})
+		return
+	}
+
+	if err := service.DB.Create(&json); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err,
+		})
+		return
+	}
+
+	var user User
+	if err := service.DB.FindUserByID(&user, json.ID); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 //ListUsers - list users in System
 func (service *UserService) ListUsers(c *gin.Context) {
-	// org := &Organization{1, "test"}
-	// jsonObject := struct {
-	// 	Users []*User `json:"users"`
-	// }{[]*User{&User{1, "Chue", "a@a.com", org}, &User{2, "Chue 2", "a@a.com", org}}}
-	c.JSON(http.StatusOK, gin.H{})
+	var users []User
+	if err := service.DB.ListUsers(&users); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, users)
+}
+
+//UpdateUser - Update a user
+func (service *UserService) UpdateUser(c *gin.Context) {
+	var json User
+	if err := c.BindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err,
+		})
+		return
+	}
+
+	if err := service.DB.Update(&json); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err,
+		})
+		return
+	}
+
+	var user User
+	if err := service.DB.FindUserByID(&user, json.ID); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
