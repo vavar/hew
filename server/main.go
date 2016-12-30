5bc3c949ba2ede4b2073b7c1e33ddb61423964fe
Warin Laocharoen <chue@abctech-thailand.com>
2016-12-30 11:30:06 +0700

    add jet

--- server/main.go
+++ server/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"./jwt"
 	"github.com/spf13/viper"
 	cors "gopkg.in/gin-contrib/cors.v1"
 	"gopkg.in/gin-gonic/gin.v1"
@@ -33,8 +35,57 @@ func main() {
 	router := gin.Default()
 	router.Use(cors.Default())
 
-	api := router.Group("/api")
+	// the jwt middleware
+	authMiddleware := &jwt.GinJWTMiddleware{
+		Realm:      "Hew Platform zone",
+		Key:        []byte("secret key"),
+		Timeout:    time.Hour,
+		MaxRefresh: time.Hour * 24,
+		Authenticator: func(userId string, password string, c *gin.Context) (string, bool) {
+			if (userId == "admin" && password == "admin") || (userId == "test" && password == "test") {
+				return userId, true
+			}
 
+			return userId, false
+		},
+		Authorizator: func(userId string, c *gin.Context) bool {
+			if userId == "admin" {
+				return true
+			}
+
+			return false
+		},
+		Unauthorized: func(c *gin.Context, code int, message string) {
+			c.JSON(code, gin.H{
+				"code":    code,
+				"message": message,
+			})
+		},
+		// TokenLookup is a string in the form of "<source>:<name>" that is used
+		// to extract token from the request.
+		// Optional. Default value "header:Authorization".
+		// Possible values:
+		// - "header:<name>"
+		// - "query:<name>"
+		// - "cookie:<name>"
+		TokenLookup: "header:Authorization",
+		// TokenLookup: "query:token",
+		// TokenLookup: "cookie:token",
+	}
+
+	router.POST("/login", authMiddleware.LoginHandler)
+	auth := router.Group("/auth")
+	auth.Use(authMiddleware.MiddlewareFunc())
+	{
+		auth.GET("/hello", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"text": "Hello World.",
+			})
+		})
+		auth.GET("/refresh_token", authMiddleware.RefreshHandler)
+	}
+
+	api := router.Group("/api")
 	var userService = NewUserService(db)
 	api.GET("/users", userService.ListUsers)
 	api.POST("/users", userService.AddUser)
